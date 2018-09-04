7a20172a1eb3dd1035f9d5a4b852e4b25352a24a
unknown <trankimsen1681998@gmail.com>
2018-09-04 20:58:40 +0700

    first commit

---
+++ angry demo/Assets/Script/birdcontroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdcontroller : MonoBehaviour {
+
+    // Use this for initialization
+    public Rigidbody2D rb;
+    private bool isPressed = false;
+    public float releaseTime =.15f;
+	// Update is called once per frame
+ 
+	void Update () {
+
+        if (isPressed)
+        {
+            rb.position =Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+	}
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;    
+    }
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        //StartCoroutine(Release());
+    }
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;                                      
+    }
+}
